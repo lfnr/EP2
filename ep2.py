90816fc9f3eaa62b4feaa343e7c7fa8f01a0ba4f
lfnr <83223802+lfnr@users.noreply.github.com>
2021-04-26 10:41:54 -0300

    Add files via upload

---
+++ ep2.py
@@ -0,0 +1,16 @@
+#Criar classe de cartas
+class Carta:
+    def __init__(self, naipe, valor):
+        self.naipe = naipe
+        self.valor = Número
+
+class Carta:
+    listaNaipes = ["Espadas", "Copas", "Ouros", "Paus"]
+    listaValores = ["Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Rainha", "Rei"]
+
+#Converte objetos (cartas) em strings
+
+def __str__(self):
+    return  (self.listaValores[self.valor]) + (self.listaNaipes[self.naipe]) 
+
+
