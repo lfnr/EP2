7e80582f66672c9d7a6f2ffab9fb093465edc61a
lfnr <83223802+lfnr@users.noreply.github.com>
2021-04-28 18:54:38 -0300

    Update EP2.py

--- EP2.py
+++ EP2.py
@@ -1,13 +1,12 @@
-class Carta:
-    def __init__(self, naipe, valor):
-        self.naipe = naipe
-        self.valor = Número
+import random
 
 class Carta:
+    #Cria uma lista de Naipes e Valores
     listaNaipes = ["♠", "♥", "♦", "♣"]
     listaValores = ["Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Rainha", "Rei"]
-
-def __str__(self):
-    return (self.listaValores[self.valor]) + (self.listaNaipes[self.naipe]) 
-
-
+    def __init__(self, naipe, valor):
+        #Inicializa os atributos que descrevem uma carta
+        self.naipe = naipe
+        self.valor = valor
+    def __str__ (self):
+        return (self.listaValores[self.valor]) + '' + (self.listaNaipes[self.naipe]) 
