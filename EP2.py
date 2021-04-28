2945898acc4ad3ed91b40ed1a7d6dd96d11b6a50
lfnr <83223802+lfnr@users.noreply.github.com>
2021-04-28 17:12:34 -0300

    Update EP2.py

--- EP2.py
+++ EP2.py
@@ -1,4 +1,3 @@
-#Criar classe de cartas
 class Carta:
     def __init__(self, naipe, valor):
         self.naipe = naipe
@@ -8,9 +7,7 @@ class Carta:
     listaNaipes = ["♠", "♥", "♦", "♣"]
     listaValores = ["Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Rainha", "Rei"]
 
-#Converte objetos (cartas) em strings
-
 def __str__(self):
-    return  (self.listaValores[self.valor]) + (self.listaNaipes[self.naipe]) 
+    return (self.listaValores[self.valor]) + (self.listaNaipes[self.naipe]) 
 
 
