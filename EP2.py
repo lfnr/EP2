70729541f5be2569a9769fecccb31808f628ccc7
lfnr <83223802+lfnr@users.noreply.github.com>
2021-05-07 18:14:08 -0300

    Update EP2.py

--- EP2.py
+++ EP2.py
@@ -1,12 +1,14 @@
 import random
 
-class Carta:
-    #Cria uma lista de Naipes e Valores
-    listaNaipes = ["♠", "♥", "♦", "♣"]
-    listaValores = ["Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-    def __init__(self, naipe, valor):
-        #Inicializa os atributos que descrevem uma carta
-        self.naipe = naipe
-        self.valor = valor
-    def __str__ (self):
-        return (self.listaValores[self.valor]) + ' ' + (self.listaNaipes[self.naipe]) 
+def cria_baralho():
+    baralho = []
+    naipes = ["♠", "♥", "♦", "♣"]
+    for valor in range(1, 14):
+        for naipe in naipes:
+            baralho.append(str(valor)+naipe)
+return baralho
+
+deck = cria_baralho()
+random.shuffle(baralho)
+
+print(baralho)
