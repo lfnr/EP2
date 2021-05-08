8f010808409b90848f2af099560ab5adb7503df0
lfnr <83223802+lfnr@users.noreply.github.com>
2021-05-08 17:13:10 -0300

    Update EP2.py

--- EP2.py
+++ EP2.py
@@ -2,11 +2,13 @@ import random
 
 def cria_baralho():
     baralho = []
+    valor = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     naipes = ["♠", "♥", "♦", "♣"]
-    for valor in range(1, 14):
+    for num in valor:
         for naipe in naipes:
-            baralho.append(str(valor)+naipe)
-return baralho
+            baralho.append(str(num)+naipe)
+    return baralho
+
 
 deck = cria_baralho()
 random.shuffle(baralho)
